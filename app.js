8999304cdc9fa2cfcbaa5d04589f20016cdbc043
Khayman King <khaymanak@gmail.com>
2022-03-08 11:30:50 -0800

    Grabbed DOM elements

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 // import functions and grab DOM elements
+const leftHatEl = document.getElementById('hat-left');
+const centerHatEl = document.getElementById('hat-center');
+const rightHatEl = document.getElementById('hat-right');
+
+const leftButtonEl = document.getElementById('button-left');
+const centerButtonEl = document.getElementById('button-center');
+const rightButtonEl = document.getElementById('button-right');
+
+const correctTotals = document.getElementById('wins');
+const attemptTotals = document.getElementById('totals');
+
+
 
 // let state
 
